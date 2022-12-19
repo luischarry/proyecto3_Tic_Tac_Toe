2c4b892feb3cf34897af0e811efa9eaac1e54cfe
luischarry <luischarry789@gmail.com>
2022-12-19 10:35:59 +0100

    se repara enlaces

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 function login() {
-    let jugador1 = document.getElementById("jugador1").value;
-    let jugador2 = document.getElementById("jugador2").value;
+    var jugador1 = document.getElementById("jugador1").value;
+    var jugador2 = document.getElementById("jugador2").value;
     if(jugador1 && jugador2){
         localStorage.setItem("jugador1",jugador1);
         localStorage.setItem("jugador2",jugador2);
